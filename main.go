118837edf75824bfe52bdff8a49eae3ec486b5c6
unknown <mohamadomr33@gmail.com>
2017-09-25 22:32:07 +0200

    working example?

--- main.go
+++ main.go
@@ -3,21 +3,35 @@ package main
 import (
 	"web-push-go/webpush"
 	"fmt"
+	"encoding/base64"
+
 )
 
 func main() {
 
-	key256 := "BClMhqu1+FmXeruyMqNfiifYWtCXG5Df4rBomMTbS+f9gZ+3L3bRbxL20y11/WFhZN6+II8vQkbOltk9GvIedNI="
-	auth   := "OGtdsqxVRRLOT3/nEv6V/Q=="
+	//key256 := "BClMhqu1+FmXeruyMqNfiifYWtCXG5Df4rBomMTbS+f9gZ+3L3bRbxL20y11/WFhZN6+II8vQkbOltk9GvIedNI="
+	//auth   := "OGtdsqxVRRLOT3/nEv6V/Q=="
 
-	// The values that make up the Subscription struct come from the browser
+	key256 , _ := base64.StdEncoding.DecodeString("BClMhqu1+FmXeruyMqNfiifYWtCXG5Df4rBomMTbS+f9gZ+3L3bRbxL20y11/WFhZN6+II8vQkbOltk9GvIedNI=")
+	auth, _ := base64.StdEncoding.DecodeString("OGtdsqxVRRLOT3/nEv6V/Q==")
+
+	//// The values that make up the Subscription struct come from the browser
+	//var exampleJSON string
+	// exampleJSON = `{"endpoint": "https://gcm-http.googleapis.com/gcm/send", "keys": {"p256dh": "BClMhqu1+FmXeruyMqNfiifYWtCXG5Df4rBomMTbS+f9gZ+3L3bRbxL20y11/WFhZN6+II8vQkbOltk9GvIedNI=", "auth": "OGtdsqxVRRLOT3/nEv6V/Q=="}}`
+	//
+	//sub, err := webpush.SubscriptionFromJSON([]byte(exampleJSON))
+	//if err != nil {
+	//	fmt.Println(err)
+	//	return
+	//}
+
 
 	sub := &webpush.Subscription{
 		Endpoint:"https://gcm-http.googleapis.com/gcm/send",
-		Key:  []byte(key256),
-		Auth: []byte(auth)}
+		Key:  key256,
+		Auth: auth}
 
-	res , err := webpush.Send(nil, sub, "hi", "AIzaSyAYLY9qTgd8M4t7yFXsGKJFtEqwp4YasfI")
+	res , err := webpush.Send(nil, sub, "hii", "AIzaSyAYLY9qTgd8M4t7yFXsGKJFtEqwp4YasfI")
 
 	if err != nil {
 		fmt.Println(err)
